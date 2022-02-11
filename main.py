4c409e0c9104ee41bbe00b5026fa726c61b443c5
Kyo/coolboy <85298936+Apoorv-create@users.noreply.github.com>
2022-02-11 20:22:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ root=Tk()
 root.minsize(650,650)
 root.maxsize(650,650)
 
-open_img = ImageTk.PhotoImage(Image.open("C:\\Users\\apoor\\Downloads\\160\\C-160\\open.png"))
-exit_img = ImageTk.PhotoImage(Image.open("C:\\Users\\apoor\\Downloads\\160\\C-160\\exit.jpg"))
-save_img = ImageTk.PhotoImage(Image.open("C:\\Users\\apoor\\Downloads\\160\\C-160\\save.png"))
+open_img = ImageTk.PhotoImage(Image.open(r"C:\C-160\open.png"))
+exit_img = ImageTk.PhotoImage(Image.open(r"C:\C-160\exit.jpg"))
+save_img = ImageTk.PhotoImage(Image.open(r"C:\C-160\save.png"))
 
 label_file_name = Label(root, text="File Name")
 label_file_name.place(relx = 0.28, rely = 0.3, anchor=CENTER)
@@ -20,13 +20,9 @@ input_file_name.place(relx = 0.46, rely = 0.3, anchor=CENTER)
 mytext = Text(root, height = 35, width = 80)
 mytext.place(relx = 0.8, rely = 0.55, anchor=CENTER)
 
-open_button = Button(root, image = open_img, text = "OpenFile", command = openFile)
-open_button.place(relx = 0.05, rely = 0.03, anchor=CENTER)
-
-
 name = ""
 
-def openFile():
+def OpenFile():
     global name
     mytext.delete(1.0, END)
     input_file_name.delete(0, END)
@@ -41,4 +37,30 @@ def openFile():
     my_text.insert(END, para)
     text_file.close()
     
+
+def save():
+    input_name = input_file_name.get()
+    file = open(input_name + ".txt", "w")
+    data = mytext.get("1.0", END)
+    print(data)
+    file.write(data)
+    input_file_name.delete(0, END)
+    mytext.delete(1.0, END)
+    messagebox.showinfo("Update", "Success")
+
+def closewindow():
+    root.destroy()
+    
+open_button = Button(root, image = open_img, text = "openFile", command = OpenFile)
+open_button.place(relx = 0.05, rely = 0.03, anchor=CENTER)
+
+save_button = Button(root, image = save_img, text = "saveFile", command = save)
+save_button.place(relx = 0.11, rely = 0.03, anchor=CENTER)
+
+exit_button = Button(root, image = exist_button, text = "closeFile", command = closeWindow)
+exit_button.place(relx = 0.17, rely = 0.03, anchor=CENTER)
+
+
+
+
 root.mainloop()
